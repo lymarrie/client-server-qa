08cae7b05147aee055638af60d86d3425c7d035d
lymarrie <lucmarrie1@gmail.com>
2023-05-30 12:54:41 -0400

    initial commit

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -1,13 +1,3 @@
-/**
- * This is an example of how to create a template that makes use of streams data.
- * The stream data originates from Yext's Knowledge Graph. When a template in
- * concert with a stream is built by the Yext Pages system, a static html page
- * is generated for every corresponding (based on the filter) stream document.
- *
- * Another way to think about it is that a page will be generated using this
- * template for every eligible entity in your Knowledge Graph.
- */
-
 import * as React from "react";
 import {
   GetHeadConfig,
@@ -29,22 +19,16 @@ import Details from "../components/Details";
 import Hours from "../components/Hours";
 import PageLayout from "../components/PageLayout";
 import EditTool from "../components/EditTool";
-import BreadCrumbs from "../components/Breadcrumbs";
 import Button from "@mui/material/Button";
+import Checkbox from '@mui/material/Checkbox';
 
-/**
- * Required when Knowledge Graph data is used for a template.
- */
+
 export const config: TemplateConfig = {
   stream: {
     $id: "location-stream",
-    // Defines the scope of entities that qualify for this stream.
-    // You can use entityTypes, savedFilterIds, and/or entityIds
     filter: {
       entityTypes: ["location"],
     },
-    // Specifies the exact data that each generated document will contain.
-    // This data is passed in directly as props to the default exported function.
     fields: [
       "id",
       "uid",
@@ -58,12 +42,8 @@ export const config: TemplateConfig = {
       "geocodedCoordinate",
       "services",
       "photoGallery",
-      "dm_directoryParents.name",
-      "dm_directoryParents.slug",
-      "dm_directoryParents.meta",
-      "dm_directoryParents.c_addressRegionDisplayName",
+      "paymentOptions"
     ],
-    // The entity language profiles that documents will be generated for.
     localization: {
       locales: ["en"],
       primary: false,
@@ -74,28 +54,13 @@ export const config: TemplateConfig = {
   },
 };
 
-/**
- * Defines the path that the generated file will live at for production.
- *
- * NOTE: To preview production URLs locally, you must return document.slug from this function
- * and ensure that each entity has the slug field pouplated.
- */
+
 export const getPath: GetPath<TemplateProps> = ({ document }) => {
   return document.slug
     ? document.slug
     : `${document.locale}/${document.address.region}/${document.address.city}/${
         document.address.line1
       }-${document.id.toString()}`;
-};
-
-/**
- * Defines a list of paths which will redirect to the path created by getPath.
- *
- * NOTE: This currently has no impact on the local dev path. Redirects will be setup on
- * a new deploy.
- */
-export const getRedirects: GetRedirects<TemplateProps> = ({ document }) => {
-  return [`index-old/${document.locale}/${document.id.toString()}`];
 };
 
 /**
@@ -131,36 +96,7 @@ export const getRedirects: GetRedirects<TemplateProps> = ({ document }) => {
 //   };
 // };
 
-/**
- * Required only when data needs to be retrieved from an external (non-Knowledge Graph) source.
- * If the page is truly static this function is not necessary.
- *
- * This function will be run during generation and pass in directly as props to the default
- * exported function.
- */
-export const transformProps: TransformProps<any> = async (data) => {
-  const { dm_directoryParents, name } = data.document;
 
-  (dm_directoryParents || []).push({ name: name, slug: "" });
-
-  return {
-    ...data,
-    document: {
-      ...data.document,
-      dm_directoryParents: dm_directoryParents,
-    },
-  };
-};
-
-/**
- * This is the main template. It can have any name as long as it's the default export.
- * The props passed in here are the direct stream document defined by `config`.
- *
- * There are a bunch of custom components being used from the src/components folder. These are
- * an example of how you could create your own. You can set up your folder structure for custom
- * components any way you'd like as long as it lives in the src folder (though you should not put
- * them in the src/templates folder as this is specific for true template files).
- */
 const Location: Template<TemplateRenderProps> = ({
   relativePrefixToRoot,
   document,
@@ -173,28 +109,34 @@ const Location: Template<TemplateRenderProps> = ({
     services,
     description,
     siteDomain,
-    dm_directoryParents,
   } = document;
 
   const [count, setCount] = React.useState(0);
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  console.log(count)
 
   return (
     <>
       <PageLayout>
         <Banner name={name} address={address} />
         <div className="centered-container">
-          <BreadCrumbs
-            breadcrumbs={dm_directoryParents}
-            baseUrl={relativePrefixToRoot}
-          />
+          <div className="section space-y-5">
+            <h1>Material UI Button</h1>
+            <div className="flex space-x-5 items-center">
+              <Button variant="outlined" color="error">Hello World</Button>
+              <div className="space-x-2">
+                <button className="font-semibold" onClick={() => setCount(c => c + 1)}>Current count:</button>
+                <span>{count}</span>
+              </div>
+            </div>
+            <Checkbox {...label} defaultChecked />
+          </div>
           <div className="grid gap-x-10 gap-y-10 md:grid-cols-2">
             <Details address={address} phone={mainPhone} services={services} />
             {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
             {description && <About name={name} description={description} />}
           </div>
-          <Button variant="outlined" color="error">Hello World</Button>
-          <button onClick={() => setCount(c => c + 1)}>Click Me</button>
-          {count}
+
         </div>
       </PageLayout>
       {/* This component displays a link to the entity that represents the given page in the Knowledge Graph*/}
